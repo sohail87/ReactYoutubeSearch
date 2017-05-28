dc321f2145e7cdc39c552e1656e7943bf5fbceff
sohail87 <sohaildeen73@gmail.com>
2017-05-28 19:34:08 +0100

    video_detail component created, used template strings

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const VideoDetail = (props) =>{
+const VideoDetail = ({video}) =>{
+  const videoId = video.Id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
   return(
-
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url} ></iframe>
+      </div>
+      <div className="details">
+        <div >{video.snippet.title}</div>
+        <div >{video.snippet.description}</div>
+      </div>
+    </div>
   );
 }
 
