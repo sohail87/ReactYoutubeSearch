e472d35254b622810a32301e0a35be0d894d46fe
sohail87 <sohaildeen73@gmail.com>
2017-05-28 15:55:12 +0100

    converted search_bar cmp from function cmp to class cmp

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+// class cmp, used when we want to have some sort of internal record keeping
+// ability for it to be aware of itself, whats happened since it rendered
+// ability to tell other components what has changed
+
+
+class SearchBar extends React.Component {
+  render(){
+    return <input />
+  }
+}
 
 export default SearchBar;
