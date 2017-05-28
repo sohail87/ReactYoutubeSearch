ae7b531d94ed2c0ffe81bdf33c17b3de83797f27
sohail87 <sohaildeen73@gmail.com>
2017-05-28 16:39:28 +0100

    handling user events - do something when user types into the text box

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // { Component } means import react and pull of the property component as a variable named component
-// syntactic sugar for... const Component = React.Component;
+// syntactic  sugar for... const Component = React.Component;
 
 // class cmp, used when we want to have some sort of internal record keeping
 // ability for it to be aware of itself, whats happened since it rendered
@@ -9,7 +9,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render(){
-    return <input />
+    return <input onChange={this.onInputChange}/>
+  }
+  onInputChange(event){
+    console.log(event.target.value);
+
   }
 }
 
