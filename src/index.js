7a7da8a213c21d44ffeb5b5f7b080f313fe2ac41
sohail87 <sohaildeen73@gmail.com>
2017-05-28 13:10:31 +0100

    create a simple component that produces HTML, but is not rendered in the DOM

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create a new component. this should produce some html
+
+const App = function () {
+  return <div>Hi!</div>;
+}
+
+// take this component's generated html and put it in the page (in the DOM)
