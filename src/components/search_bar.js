e506432a74f43466b7baef25568cb6d45cdd11a5
sohail87 <sohaildeen73@gmail.com>
2017-05-28 16:42:10 +0100

    convert function call to inline arrow function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,7 +9,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render(){
-    return <input onChange={this.onInputChange}/>
+    return <input onChange={event => console.log(event.target.value)}/>
   }
   onInputChange(event){
     console.log(event.target.value);
