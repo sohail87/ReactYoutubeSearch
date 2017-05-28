ed0e68597b356bbcbeeab97a7c488e5ffbfb4c53
sohail87 <sohaildeen73@gmail.com>
2017-05-28 16:01:46 +0100

    pull of the property component as a variable named component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+// { Component } means import react and pull of the property component as a variable named component
+// syntactic sugar for... const Component = React.Component;
 
 // class cmp, used when we want to have some sort of internal record keeping
 // ability for it to be aware of itself, whats happened since it rendered
 // ability to tell other components what has changed
 
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render(){
     return <input />
   }
