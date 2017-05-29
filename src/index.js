b7133e42e3ef5138f23af34582e552b6b787c880
sohail87 <sohaildeen73@gmail.com>
2017-05-29 13:16:24 +0100

    throttling search term input using 'lodash'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 //go find the library 'react' installed in node_modules
 //and assign it to variable React
 import React, { Component } from 'react'; //core react library creates and manages components
@@ -31,9 +33,10 @@ class App extends Component{
   }
 
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300)
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
